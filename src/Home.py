f4216c637eab3278b06a825a3c6dd4a60b846a3d
chinesewebman <chinesewebman@163.com>
2023-09-20 15:55:56 +0800

    restore error handling

--- src/Home.py
+++ src/Home.py
@@ -62,39 +62,39 @@ else:
         st.session_state['dict'] = utils.load_dict('user-dict.json')
         #st.sidebar.markdown("### 不良回应数:"+str(st.session_state['bad_attitude_times']))
         sidebar.show_file_selecotr(st.session_state['data_directory'],history)    
-        #try:
-        chatbot = utils.setup_chatbot(
-            st.session_state["selected_file"], st.session_state["model"], st.session_state["temperature"]
-        )
-        st.session_state["chatbot"] = chatbot
+        try:
+            chatbot = utils.setup_chatbot(
+                st.session_state["selected_file"], st.session_state["model"], st.session_state["temperature"]
+            )
+            st.session_state["chatbot"] = chatbot
 
-        if st.session_state["ready"]:
-            # Create containers for chat responses and user prompts
-            response_container, prompt_container = st.container(), st.container()
-            with prompt_container:
-                # Display the prompt input box
-                is_ready, user_input = layout.prompt_input()
-                # Initialize the chat history
-                history.initialize(st.session_state["selected_file"])
-                # Reset the chat history if button clicked
-                if st.session_state["reset_chat"]:
-                    history.reset(st.session_state["selected_file"])
-                if is_ready:
-                    # Update the chat history and display the chat messages
-                    history.append("user", user_input)
-                    old_stdout = sys.stdout
-                    sys.stdout = captured_output = StringIO()
-                    # Generate the chatbot's response
-                    output = st.session_state["chatbot"].check_chat(user_input)
-                    sys.stdout = old_stdout
-                    history.append("assistant", output)
-                    # Clean up the agent's thoughts to remove unwanted characters
-                    thoughts = captured_output.getvalue()
-                    cleaned_thoughts = re.sub(r'\x1b\[[0-9;]*[a-zA-Z]', '', thoughts)
-                    cleaned_thoughts = re.sub(r'\[1m>', '', cleaned_thoughts)
-                    # Display the agent's thoughts
-                    with st.expander("显示 agent 的想法"):
-                        st.write(cleaned_thoughts)
-            history.generate_messages(response_container)
-        #except Exception as e:
-        #    st.error(f"Error: {str(e)}")
+            if st.session_state["ready"]:
+                # Create containers for chat responses and user prompts
+                response_container, prompt_container = st.container(), st.container()
+                with prompt_container:
+                    # Display the prompt input box
+                    is_ready, user_input = layout.prompt_input()
+                    # Initialize the chat history
+                    history.initialize(st.session_state["selected_file"])
+                    # Reset the chat history if button clicked
+                    if st.session_state["reset_chat"]:
+                        history.reset(st.session_state["selected_file"])
+                    if is_ready:
+                        # Update the chat history and display the chat messages
+                        history.append("user", user_input)
+                        old_stdout = sys.stdout
+                        sys.stdout = captured_output = StringIO()
+                        # Generate the chatbot's response
+                        output = st.session_state["chatbot"].check_chat(user_input)
+                        sys.stdout = old_stdout
+                        history.append("assistant", output)
+                        # Clean up the agent's thoughts to remove unwanted characters
+                        thoughts = captured_output.getvalue()
+                        cleaned_thoughts = re.sub(r'\x1b\[[0-9;]*[a-zA-Z]', '', thoughts)
+                        cleaned_thoughts = re.sub(r'\[1m>', '', cleaned_thoughts)
+                        # Display the agent's thoughts
+                        with st.expander("显示 agent 的想法"):
+                            st.write(cleaned_thoughts)
+                history.generate_messages(response_container)
+        except Exception as e:
+            st.error(f"Error: {str(e)}")
